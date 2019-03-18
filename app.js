6da7ae7521e86ef0f7a9457186062a0a30dcae75
Riunshow <zhubotaigg@gmail.com>
2019-03-18 16:30:02 +0800

    流名称由填写roomID改为填写token

--- app.js
+++ app.js
@@ -13,6 +13,19 @@ const config = {
   http: {
     port: 9090,
     allow_origin: '*'
+  },
+  trans: {
+    ffmpeg: '/Users/zhubotai/Documents/class/school/ffmpeg/ffmpeg',
+    tasks: [{
+      app: 'live',
+      ac: 'aac',
+      hls: true,
+      hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+      dash: true,
+      dashFlags: '[f=dash:window_size=3:extra_window_size=5]',
+      mp4: true,
+      mp4Flags: '[movflags=faststart]',
+    }]
   }
 };
 
@@ -21,14 +34,27 @@ nms.run();
 
 
 nms.on('postPublish', (id, StreamPath, args) => {
-  let roomID = StreamPath.match(/\d+/g);
-  streamArr[id] = roomID;
-  axios.get(`http://${serverAddr}/api/live/start/${roomID}`).then((res ) => {
-    console.log(res.data);
-  })
-  .catch((err) => {
+  let token = StreamPath.split('/')[2]
+
+  axios.post(`http://${serverAddr}/api/live/getRoomIdByToken`, {
+    token
+  }).then(res => {
+    console.log(res.data.data);
+    let roomID = res.data.data.roomID
+    streamArr[id] = roomID;
+  
+    axios.get(`http://${serverAddr}/api/live/start/${roomID}`).then((res ) => {
+      console.log(res.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+
+  }).catch((err) => {
     console.log(err);
-  });
+  })
+
+
 });
 
 nms.on('donePublish', (id, StreamPath, args) => {
